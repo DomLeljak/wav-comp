9643d6b01e90470b1bd01c0dd9f038e4a0f04098
DomLeljak <domagojleljak@gmail.com>
2021-10-27 17:03:12 +0200

    [infra] Adapt the generate.cc script to generate a more complex dataset with  trigonometric properties

--- generate.cc
+++ generate.cc
@@ -13,18 +13,30 @@ int main()
 	typedef boost::mt19937 base_generator_type;
 	base_generator_type generator(time(0));
 	boost::random::uniform_real_distribution<> uni_dist(-1.,1.);
+	boost::random::uniform_real_distribution<> uni_dist_2(.80, 1.2);
 	boost::variate_generator<base_generator_type&, boost::random::uniform_real_distribution<> > uni(generator, uni_dist);
+	boost::variate_generator<base_generator_type&, boost::random::uniform_real_distribution<> > uni_2(generator, uni_dist_2);
 
-	int N;
+	int Nx, Ns;
+	int periods = 8; // number of periods
 	std::cout << "Number of data points: ";
-	std::cin >> N;
-	float* data = new float[N];
+	std::cin >> Nx;
+	std::cout << "Number of data sets: ";
+	std::cin >> Ns;
+
+	float* data = new float[Ns * Nx];
+
 	if (data == nullptr)
 		std::cout << "Error: memory could not be allocated";
 
-	for (int x = 0; x < N; x++){
-		data[x] = sin (x * 2 * M_PI / N) + 0.1 * uni();
-		printf("%4f \n", data[x]);
+	for (int i = 0; i < Ns; i++){
+		for (int x = 0; x < Nx; x++){
+			data[Nx*i + x] = uni_2() * sin (2 * x * periods * 2 * M_PI / Nx)
+				* uni_2() * std::sqrt(x) * sin (x * periods * 2 * M_PI / Nx)
+				+ 0.1 * std::sqrt(x) *  uni()
+				+ std::sqrt(x);
+			printf("%d, %d: %4f \n", i, Nx, data[x]);
+		}
 	}
 
 	{
@@ -35,10 +47,13 @@ int main()
 			std::cerr << "can't open output file" << std::endl;
 		}
 
-		for (int x = 0; x < N; x++){
-			myfile << std::fixed << std::setprecision(4) << x * 2 * M_PI / N << "	" << data[x] << std::endl;
+		for (int x = 0; x < Nx; x++){
+			myfile << std::fixed << std::setprecision(4) << x * periods * 2 * M_PI / Nx;
+			for (int i = 0; i < Ns; i++){
+				myfile << "	" << data[i*Nx + x];
+			}
+			myfile << std::endl;
 		}
-
 		myfile.close();
 		
 		return 0;
@@ -46,7 +61,6 @@ int main()
 	}
 	
 	delete[] data;
-	data = NULL;
 
 	return 0;
 
